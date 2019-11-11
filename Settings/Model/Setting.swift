7b7cc10085f707cc5291e36ecb5d551d60292eb8
Karl Pfister <pfister@Karls-MacBook-Pro.local>
2019-11-11 14:33:17 -0700

    Updates to xCode 11.2 / Swift 5
    • Adds updated file stucture
    • Adds deailed comments in markdown
    • Updated project with work with xCode 11.2 and Swift 5

--- Settings/Model/Setting.swift
+++ Settings/Model/Setting.swift
@@ -2,21 +2,27 @@
 //  Setting.swift
 //  Settings
 //
-//  Created by Karl Pfister on 4/9/19.
+//  Created by Karl Pfister on 11/11/19.
 //  Copyright © 2019 Karl Pfister. All rights reserved.
 //
 
-
 import UIKit
 
+/**
+Creates our Setting object - *MODEL*
+ - Properties:
+    - name: The *Sring* identifer for the Setting.
+    - icon: The *UIImage* that matches the Setting.
+    - isOn: The *Bool* to designiate whether the setting is on or not.
+ */
 class Setting {
-    
-    let name: String
+
+    let settingName: String
     let icon: UIImage
     var isOn: Bool
-    
+
     init(name: String, icon: UIImage, isOn: Bool) {
-        self.name = name
+        self.settingName = name
         self.icon = icon
         self.isOn = isOn
     }
